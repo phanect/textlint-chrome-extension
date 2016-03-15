9ccaf1178ac98a8c04663f105b30d6c753b9c8f7
io-monad <iride.monad@gmail.com>
2016-03-16 02:17:35 +0900

    Change license from MIT to GNU GPLv3

--- app/scripts/lib/util/strip-default.js
+++ app/scripts/lib/util/strip-default.js
@@ -1,3 +1,5 @@
+/* Copyright (C) 2016  IRIDE Monad <iride.monad@gmail.com>
+ * License: GNU GPLv3 http://www.gnu.org/licenses/gpl-3.0.html */
 "use strict";
 
 export default function stripDefault(module) {
