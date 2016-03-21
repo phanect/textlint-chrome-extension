64b01642c960f1eea521f7004f0c089882a29ab9
nodaguti <nodaguti@gmail.com>
2016-03-21 14:29:33 +0900

    Expand globs in web_accessible_resources of manifest.json
    Firefox currently does not support wildcards in web_accessible_resources key.
    (https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Chrome_incompatibilities#web_accessible_resources)
    However, listing all resources individually will require too high maintenance cost,
    so I've decided to transform the wildcards into expanded pathes during the 'manifest' task.

--- tasks/manifest.js
+++ tasks/manifest.js
@@ -2,9 +2,36 @@ import gulp from 'gulp';
 import gulpif from 'gulp-if';
 import livereload from 'gulp-livereload';
 import args from './lib/args';
+import glob from 'glob';
+import through from 'through2';
+import _ from 'lodash';
+import path from 'path';
+
+function expandWebAccessibleResources() {
+  return through.obj(function (file, enc, callback) {
+    if (!file.isBuffer()) {
+      return callback();
+    }
+
+    const manifest = JSON.parse(file.contents.toString('utf8'));
+    const cwd = path.join(process.cwd(), 'dist');
+    const expanded = _
+      .chain(manifest.web_accessible_resources)
+      .map((pattern) => glob.sync(pattern, { cwd }))
+      .flatten()
+      .value();
+
+    manifest.web_accessible_resources = expanded;
+    file.contents = new Buffer(JSON.stringify(manifest));
+    this.push(file);
+
+    return callback();
+  });
+}
 
 gulp.task('manifest', () => {
   return gulp.src('app/manifest.json')
+    .pipe(expandWebAccessibleResources())
     .pipe(gulp.dest('dist'))
     .pipe(gulpif(args.watch, livereload()));
 });
