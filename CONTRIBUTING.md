b6d91f9076ed5e971e2b86ef3cf75d8f8ec8cef5
io-monad <iride.monad@gmail.com>
2016-05-09 00:12:10 +0900

    chore: Automate versioning and release with semantic-release
    After this commit, all releases are now handled by semantic-release.
    It handles versioning, publishing package to NPM, git tags and release notes.
    `gulp release` task will make a release archive and upload it to Chrome Web Store.
    gulp tasks that bumps version are deprecated and removed.
    Introduce commit message validation.

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -6,8 +6,8 @@ First of all, thank you for your contribution on this repository! :two_hearts:
 
 Before making a pull request, please take a few minutes to read following notes:
 
-- You should `gulp test` to see if you changes break something. `gulp test` does testing by Karma and linting by eslint on your code.
-    - If you are too lazy to run gulp, never mind. PR will be checked by Travis CI anyway.
+- We are using [AngularJS's commit message convention](https://github.com/angular/angular.js/blob/master/CONTRIBUTING.md#-git-commit-guidelines).
+- Your commits and pushes are validated by git hooks.
 - You are still the copyright owner of your contribution. However, according to GPLv3 license, your contribution becomes public for anyone and licensed under GPLv3 as well.
     - The header comment in each file shows that copyright is owned by me (IRIDE Monad), but it does not mean your copyright is taken, restricted, removed or something.
 - I will put your name on [CREDITS.md](CREDITS.md).
@@ -25,8 +25,8 @@ Thank you for reading, and don't hesitate to ask me if you have any question, re
 
 プルリクエストを送る前にいくつか注意点がありますので、ご一読ください。
 
-- `gulp test` を実行して何か問題かないか確認して頂けるとありがたいです。`gulp test` は Karma によるユニットテストと eslint によるコードスタイルの確認をします。
-    - もし面倒でしたらそのままでも構いません。プルリクエストは自動的に Travis CI でチェックされます。
+- [AngularJS のコミットメッセージ規約 (英語)](https://github.com/angular/angular.js/blob/master/CONTRIBUTING.md#-git-commit-guidelines) を採用しています。
+- あなたのコミット及びリポジトリへのプッシュは git のフック機能によって自動的に検証されます。
 - あなたが作成・修正したコードの著作権はあなたにあります。しかしながら、GPLv3 ライセンス条項に従い、あなたのコードは自動的に公開され、同様に GPLv3 でライセンスされます。
     - 各ファイル中のヘッダーコメントとして、著作権の所有者が私 (IRIDE Monad) にあると書いてありますが、これはあなたの著作権が取られたり、制限されたり、除かれたりする事を意味するわけではありません。
 - あなたの名前は [CREDITS.md](CREDITS.md) に掲載されます。
---
+++ tasks/release.js
@@ -0,0 +1,69 @@
+import fs from "fs";
+import gulp from "gulp";
+import { colors, log } from "gulp-util";
+import gulpSequence from "gulp-sequence";
+import zip from "gulp-zip";
+import deploy from "chrome-extension-deploy";
+import packageJson from "../package.json";
+import args from "./lib/args";
+
+function getZipFilename() {
+  const name = packageJson.name.replace(/^@[\w-]+\//, "");
+  const version = packageJson.version;
+  const extname = args.vendor === "moz" ? ".xpi" : ".zip";
+  return `${name}-${version}-${args.vendor}${extname}`;
+}
+
+gulp.task("release", gulpSequence(
+  "release:version",
+  "release:pack",
+  "release:upload"
+));
+
+gulp.task("release:version", () => {
+  const newVersion = packageJson.version;
+  const manifest = JSON.parse(fs.readFileSync("app/manifest.json"));
+  manifest.version = newVersion;
+  fs.writeFileSync("app/manifest.json", JSON.stringify(manifest, null, 2));
+  log("Updated manifest.json with version", colors.cyan(newVersion));
+});
+
+gulp.task("pack", ["release:pack"]);
+gulp.task("release:pack", ["build"], () => {
+  const filename = getZipFilename();
+  return gulp.src(`dist/${args.vendor}/**/*`)
+    .pipe(zip(filename))
+    .pipe(gulp.dest("./packages"))
+    .on("end", () => {
+      const distStyled = colors.magenta(`dist/${args.vendor}`);
+      const filenameStyled = colors.magenta(`packages/${filename}`);
+      log(`Packed ${distStyled} to ${filenameStyled}`);
+    });
+});
+
+gulp.task("release:upload", (done) => {
+  if (args.vendor !== "chrome") {
+    log("Skipped (Target vendor is not `chrome`)");
+    done();
+    return;
+  }
+  if (!process.env.WEBSTORE_CLIENT_ID ||
+      !process.env.WEBSTORE_CLIENT_SECRET ||
+      !process.env.WEBSTORE_REFRESH_TOKEN) {
+    throw new Error("Credential for Chrome Web Store is not set");
+  }
+
+  const filename = `packages/${getZipFilename()}`;
+  deploy({
+    clientId: process.env.WEBSTORE_CLIENT_ID,
+    clientSecret: process.env.WEBSTORE_CLIENT_SECRET,
+    refreshToken: process.env.WEBSTORE_REFRESH_TOKEN,
+    id: packageJson.chromeExtensionId,
+    zip: fs.readFileSync(filename),
+  })
+  .then(() => {
+    log(`Uploaded ${colors.magenta(filename)} to Chrome Web Store`);
+    done();
+  })
+  .catch(done);
+});
